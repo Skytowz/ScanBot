7010a5a700df7c6de9cf82206e7a8b717a242218
Skytowz <lucashottin359@gmail.com>
2022-05-24 13:26:08 +0200

    duplicate variable who provoc crash of the application

--- Commandes/og.js
+++ Commandes/og.js
@@ -1,13 +1,17 @@
 const { hasRole } = require("../utils/roles");
 module.exports.run = async(client, message, args) =>{ 
-    const role = 963735030738341888;
+    const roles = 963735030738341888;
     if(message.guildId != 670239141996265473) return;
-    if(!hasRole(message.member,role)) return message.channel.send("Vous n'avez pas la permission de faire ça");
+    if(!hasRole(message.member,roles)) return message.channel.send("Vous n'avez pas la permission de faire ça");
     if(message.mentions.users.size < 1) return message.channel.send("Veuillez mentionnez une personne");
     if(message.mentions.users.size > 1) return message.channel.send("Veuillez à ne mentionnez qu'une seule personne");
     if(args.length > 2) return message.channel.send("Veuillez ne pas rajouter d'information inutile");
-    if(hasRole(message.mentions.members.first(),role)) return message.channel.send("Cet utilisateur possède déjà ce role");
-    message.mentions.members.first().roles.add(message.guild.roles.cache.find(role => role.id == role));
+    if(hasRole(message.mentions.members.first(),roles)) return message.channel.send("Cet utilisateur possède déjà ce role");
+    try{
+        message.mentions.members.first().roles.add(message.guild.roles.cache.find(role => role.id == roles));
+    }catch{
+        message.channel.send("Une erreur est survenue");
+    }
 
 };
 module.exports.help = {
